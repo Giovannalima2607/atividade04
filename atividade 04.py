5048472766916ee8c53bcfc4b579d834a92eb78a
Giovannalima2607 <giovannasilva123@gmail.com>
2024-09-10 19:38:33 -0300

    digite a nota

---
+++ atividade 04.py
@@ -0,0 +1,6 @@
+# Crie um programa que receba três notas de um aluno e calcule a média aritmética.
+n1 = float(input("digite a nota"))
+n2 = float(input("digite a nota"))
+n3 = float(input("digite a nota"))
+media = (n1+n2+n3)/3
+print(f"media e {media}")
